fb30353e71026d78abd3c2c82166cc4791a75e96
vadlasireesha <vadlasireesha208@gmail.com>
2025-12-03 13:45:47 +0530

    Add files via upload

---
+++ fibonacci series functions.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int fibonacci(int n)
+{
+	if(n<=1)
+	return n;
+	else
+	return fibonacci(n-1)+fibonacci(n-2);
+}
+int main()
+{
+	int n,i;
+	printf("enter a number:");
+	scanf("%d",&n);
+	printf("fibonacci serial:");
+	for(i=0;i<n;i++)
+	{
+		printf("%d",fibonacci(i));
+	}
+	return 0;
+}
