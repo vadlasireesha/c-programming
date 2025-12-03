8fe41b80a471bc5c05c3815fb4ee34476a43928c
vadlasireesha <vadlasireesha208@gmail.com>
2025-12-03 13:48:12 +0530

    Add files via upload

---
+++ keyword static.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	static int a=10;
+	a++;
+	return a;
+}
+int main()
+{
+	printf("value of a: %d\n",display());
+		printf("value of a: %d\n",display());
+printf("value of a: %d\n",display());
+return 0;
+}
