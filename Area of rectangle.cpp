1042d9360b173d3d827143b1c78389d4cbe9be68
vadlasireesha <vadlasireesha208@gmail.com>
2025-12-03 13:42:17 +0530

    Add files via upload

---
+++ Area of rectangle.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int L,A,P;
+	printf("enter the length of a square L:");
+	scanf("%d",& L);
+	A=L*L;
+	P=4*L;
+	printf("area=%d/n",A);
+	printf("perimeter=%d",P);
+	return 0;
+}
+	
