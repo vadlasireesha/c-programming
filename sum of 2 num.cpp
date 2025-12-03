93d53a5555d295e0064ae95cd43bd44a18f6e7bf
vadlasireesha <vadlasireesha208@gmail.com>
2025-12-03 13:52:58 +0530

    Add files via upload

---
+++ sum of 2 num.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter a value of a:");
+	scanf("%d",& a);
+	printf("enter a value of b:");
+	scanf("%d",& b);
+	c=a+b;
+	printf("sum of two numbers is:%d",c);
+	return 0;
+	
+}
