b1d20fc2d9730cc56c9c748240e9613683bcde9e
vadlasireesha <vadlasireesha208@gmail.com>
2025-12-03 13:41:42 +0530

    Add files via upload

---
+++ area & perimeter of a square.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int A,B,C;
+	float average;
+	printf("enter value of A,B,C:/n");
+	scanf("%d %d %d",& A,&B,&C);
+	average = (A + B + C) / 3.0;
+	printf("average of given 3 numbers is:%f",average);
+	return 0;
+	
+}
+	
