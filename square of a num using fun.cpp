27961229d838be6c8352e62491c5e771b8b0f4d3
vadlasireesha <vadlasireesha208@gmail.com>
2025-12-04 15:11:16 +0530

    Add files via upload

---
+++ square of a num using fun.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int square(int n) {
+ return n * n;
+int main()
+ int num;
+ print("Enter a number: ");
+ scanf("%d", &num);
+ print("Square = %d\n", square(num));
+ return 0;
+}
